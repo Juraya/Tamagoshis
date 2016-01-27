b8c733d4e9df336fc18ed734c98c8ce001a5da43
Julien <julien.paris.casset@gmail.com>
2016-01-27 11:42:12 +0100

    Ajout images

--- src/tamagoshi/jeu/Main.java
+++ src/tamagoshi/jeu/Main.java
@@ -1,5 +1,7 @@
 package tamagoshi.jeu;
 
+import tamagoshi.tamafenetre.Fenetre;
+
 /**
  * Created by Julien on 05/10/2015.
  * Classe utilisée pour lancer le jeu.
@@ -7,9 +9,11 @@ package tamagoshi.jeu;
 public class Main {
     public static void main(String[] args) {
         // On créé une nouvelle partie.
-        TamaGame jeu = new TamaGame();
+        //TamaGame jeu = new TamaGame();
+        Fenetre fenetre = new Fenetre();
+
 
         // On lance cette partie.
-        jeu.play();
+        //jeu.play();
     }
 }
